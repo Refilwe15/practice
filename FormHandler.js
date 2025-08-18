248de06d6865b0a1a1d0f9d8504b725446e83321
Refilwe15 <mrefilwe880@gmail.com>
2025-08-18 16:28:24 +0200

    commits

---
+++ FormHandler.js
@@ -0,0 +1,12 @@
+class FormHandler{
+    constructor(name,email,password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+   
+    validateEmail(){
+
+    }
+}
